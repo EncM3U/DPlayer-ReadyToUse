e12ca67387f6bfeed3ae8e239b3b182326aa67cb
MoChanBW <bryanw@vip.qq.com>
2020-04-10 18:40:22 +0800

    Update function base64Decoder

--- assets/js/webMainLogic.js
+++ assets/js/webMainLogic.js
@@ -201,3 +201,28 @@ function checkMobileLandscape() {
         screen.orientation.lock("landscape");
     }
 }
+
+function base64Decoder(encodedString){  
+    let Base64 = {                                                  //from https://www.jianshu.com/p/82afa633033e
+    encode(str) {
+        // first we use encodeURIComponent to get percent-encoded UTF-8,
+        // then we convert the percent encodings into raw bytes which
+        // can be fed into btoa.
+        return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+            function toSolidBytes(match, p1) {
+                return String.fromCharCode('0x' + p1);
+            }));
+    },
+    decode(str) {
+        // Going backwards: from bytestream, to percent-encoding, to original string.
+        return decodeURIComponent(atob(str).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    }
+};
+
+    let encoded = Base64.encode(encodedString); // "5ZOIaGE="
+    let decoded = Base64.decode(encoded); // "哈ha"
+    return decoded;
+}
+
