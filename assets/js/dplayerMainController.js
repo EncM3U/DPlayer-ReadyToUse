97282738166e11419d454490aaa3a4a2de6e304a
MoChanBW <bryanw@vip.qq.com>
2020-05-06 09:59:43 +0800

    Update dplayerMainController.js

--- assets/js/dplayerMainController.js
+++ assets/js/dplayerMainController.js
@@ -1,12 +1,11 @@
 const dp = new DPlayer({ //DPlayer主控制函数,详见 http://dplayer.js.org
     container: document.getElementById('dplayer'),
-    screenshot: true,
+    screenshot: getTrueorF("screenshot"),
     live: getTorFalse("live"), //是否开启直播模式
     autoplay: getTorFalse("autoplay"), //自动播放 参数autoplay 值为1或0 默认false
     theme: '#8470FF',
-    loop: false,
+    loop: getTorFalse("loop"),
     lang: getLang(), //DPlayer 自带有获取browser lang的功能，这个函数可以给自定义
-    screenshot: true,
     hotkey: true,
     preload: getVariable("preload"),
     logo: getVariable("playerlogo"), //67% 不透明度左上角icon
@@ -49,14 +48,15 @@ const dp = new DPlayer({ //DPlayer主控制函数,详见 http://dplayer.js.org
 });
 
 //修复手机横屏问题  from https://dandoc.u2sb.top/danmu/install.html#dplayer-%E7%9A%84%E7%AE%80%E5%8D%95%E5%BA%94%E7%94%A8
-dp.on("fullscreen", function () {
+/*dp.on("fullscreen", function () {
     fullScreenNow = 1;
     if (
         /Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     ) {
         screen.orientation.lock("landscape");
     }
-});
+});*/
+
 /*
 dp.on("fullscreen_cancel", function () {
     fullScreenNow = 0;
